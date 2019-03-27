38e17c8d2a69d22963af3c58033dedd4503b2ba8
Vladislav Oshkanov <ovs@live.ru>
2019-03-27 18:24:01 +0700

    identication of constants-situations implemented

--- TwoPosPredicate.py
+++ TwoPosPredicate.py
@@ -8,3 +8,5 @@ class TwoPosPredicate:
         self.value = value
     def print(self):
         print("{}(A{}, {})".format(self.name, self.constant_situation, self.value))
+    def to_string(self):
+        return "{}(A{}, {})".format(self.name, self.constant_situation, self.value)
--- UI.py
+++ UI.py
@@ -31,7 +31,7 @@ class MainApp(App):
         self.text = InputText.InputText()
         # s = Scatter(pos_hint={'x': .1 ,'y': .2 },
                     # size_hint=(.2, .8))
-        ti = TextInput(pos_hint={'x': .1 ,'y': .7 }, size_hint=(.8, .2))
+        ti = TextInput(text = 'Вася вчера купил у Пети красный мотоцикл за 10 рублей.', pos_hint={'x': .1 ,'y': .7 }, size_hint=(.8, .2))
         # s.add_widget(ti)
         def on_text(instance, value):
             self.text.setText(value)
@@ -54,20 +54,41 @@ class MainApp(App):
 
             resultsLayout = GridLayout(size=(1000, 1000), cols = 1, spacing = 10, size_hint_y = None)
             results.add_widget(resultsLayout)
+
+            checkboxes_refs = {}
+            labels_refs = {}
             for index, predicate in enumerate(two_pos_predicates):
                 result = GridLayout(size=(300, 300), rows=1)
-                my_label = Label(text = predicate.name)
+                my_label = Label(text = predicate.to_string())
+                labels_refs[str(index)] = {"label": my_label, "pred": predicate}
                 result.add_widget(my_label)
-                result.add_widget(CheckBox())
-                with result.canvas.before:
-                    Color(0, 1, 0, 1) # green; colors range from 0-1 instead of 0-255
-                    result.rect = Rectangle(size=result.size,
-                                pos=result.pos)
-                resultsLayout.add_widget(result)
+                cb = CheckBox()
+                checkboxes_refs["CB" + str(index)] = {"checkbox": cb, "cs": predicate.constant_situation};            
+                result.add_widget(cb)
         
-        btn = Button(text='Process', pos_hint={'x': .1, 'y': .1}, size_hint=(.8, .2),
+                resultsLayout.add_widget(result)
+            def on_cs_btn_release(arg):
+                cs_set = set()
+                for idx, cb in checkboxes_refs.items():
+                    print(cb["checkbox"].active)
+                    if (cb["checkbox"].active):
+                        cs_set.add(cb["cs"])
+                print(cs_set)
+                if (len(cs_set) > 1): 
+                    for predicate in two_pos_predicates:
+                        if predicate.constant_situation in cs_set:
+                            predicate.constant_situation = list(cs_set)[0]
+                    
+                for idx, label in labels_refs.items():
+                    label["label"].text = label["pred"].to_string()
+
+            btn_cs = Button(text='Объединить константы-ситуации', pos_hint={'x': .1, 'y': .1}, size_hint=(.8, .2),
+                halign='center', on_release=on_cs_btn_release)
+            self.root.add_widget(btn_cs)
+        
+
+        btn = Button(text='Обработать текст', pos_hint={'x': .1, 'y': .1}, size_hint=(.8, .2),
                 halign='center', on_release=on_process_btn_release)
-       
         # btn.bind(on_release=release_all_keyboard)
         self.root.add_widget(btn)
 
